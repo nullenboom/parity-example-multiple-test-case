71529be39877d956619325b8a9111f66d5e51966
Niklas Ullenboom <niklas.ullenboom@paluno.uni-due.de>
2020-01-04 14:49:54 +0100

    Add example truffle project

---
+++ test/Non-Parity Numbers Scenario.js
@@ -0,0 +1,17 @@
+const Binary = artifacts.require("./ParityBinary.sol");
+const Modulo = artifacts.require("./ParityModulo.sol");
+
+contract("Parity_Binary", async accounts => {
+  it("Non-Parity Numbers Scenario", async () => {
+    const binary = await Binary.new();
+    const parity = await binary.checkParity(10, 5);
+  });
+});
+
+
+contract("Parity_Modulo", async accounts => {
+  it("Non-Parity Numbers Scenario", async () => {
+    const modulo = await Modulo.new();
+    const parity = await modulo.checkParity(10, 5);
+  });
+});
